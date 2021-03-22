6f1944bd2d71e7b9146110a5ab9c1e04096f56e1
Dhruv Kumar Patwari <dhruv.patwari@zemosolabs.com>
2021-03-22 16:01:32 +0530

    Completed assignment 2

---
+++ assignments/src/com/zemoso/assignment2/AllLettersPresent.java
@@ -0,0 +1,32 @@
+package com.zemoso.assignment2;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AllLettersPresent {
+    // Space remains constant irrespective of the input size. Hence space complexity is o(1)
+    private static HashMap<Character, Integer> letterFrequency = new HashMap<>();
+    private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        initHashMap();
+        inputString = inputString.toLowerCase();
+        char[] inputStringLetters = inputString.toCharArray();
+        for(char c: inputStringLetters){ // o(n) here n is the size of the string.
+            if(alphabet.contains(String.valueOf(c)))
+                letterFrequency.put(c, letterFrequency.get(c) + 1);
+        }
+        if(letterFrequency.containsValue(0))
+            System.out.println(false);
+        else
+            System.out.println(true);
+    }
+
+    private static void initHashMap(){
+        char[] alphabetLetters = alphabet.toCharArray();
+        for(char c : alphabetLetters) // o(1) as this is not dependant on the input size.
+            letterFrequency.putIfAbsent(c, 0);
+    }
+}
