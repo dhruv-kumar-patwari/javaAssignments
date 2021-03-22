2116ad30758b57f999c6d854edcf47b048daf870
Dhruv Kumar Patwari <dhruv.patwari@zemosolabs.com>
2021-03-22 15:18:42 +0530

    Assignment 1

---
+++ assignments/src/com/zemoso/assignment1/HomeDirectoryCrawler.java
@@ -0,0 +1,25 @@
+package com.zemoso.assignment1;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class HomeDirectoryCrawler {
+    public static void main(String[] args) {
+        File directory = new File("/home/dhrkp/");
+        String[] filesInDir = directory.list();
+        String regex;
+        while (true){
+            System.out.println("Enter regular expression or type q to quit");
+            Scanner scanner = new Scanner(System.in);
+            regex = scanner.next();
+            if(regex.equals("q"))
+                break;
+            else{
+                for (String file: filesInDir){
+                    if(file.matches(regex))
+                        System.out.println(file);
+                }
+            }
+        }
+    }
+}
