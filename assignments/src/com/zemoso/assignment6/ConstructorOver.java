092aa1716a147fb998be3e388054d27fcae01bcc
Dhruv Kumar Patwari <dhruv.patwari@zemosolabs.com>
2021-03-23 19:54:12 +0530

    Assignment 6 added

--- assignments/src/com/zemoso/assignment6/ConstructorOver.java
+++ assignments/src/com/zemoso/assignment6/ConstructorOver.java
@@ -1,16 +1,27 @@
 package com.zemoso.assignment6;
 
+import java.util.ArrayList;
+
 public class ConstructorOver {
     String fieldValue;
 
-//    public ConstructorOver() {
-//        System.out.println("Constructor 1");
-//        ConstructorOver("Hello");
-//    }
-//
-//    public ConstructorOver(String fieldValue) {
-//        this.fieldValue = fieldValue;
-//        System.out.println("Constructor 2");
-//        this.ConstructorOver();
-//    }
+    public ConstructorOver() {
+        this("String to be printed");
+        System.out.println("Constructor 1");
+    }
+
+    public ConstructorOver(String fieldValue) {
+        this.fieldValue = fieldValue;
+        System.out.println("Constructor 2");
+        System.out.println(fieldValue);
+    }
+
+    public static void main(String[] args) {
+        ConstructorOver obj1=new ConstructorOver();
+        ConstructorOver obj2[]=new ConstructorOver[2];
+
+        obj2[0]=new ConstructorOver("Dhruv");
+        obj2[1]=new ConstructorOver("Test");
+    }
+
 }
